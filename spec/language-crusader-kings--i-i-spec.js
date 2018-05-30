856309c377369458786c1d3bdc930aae9f22465e
howlinweed <coolguy69420@local7.info>
2018-05-30 13:57:02 -0400

    Add bare minumum to spec and add travis support.

--- spec/language-crusader-kings--i-i-spec.js
+++ spec/language-crusader-kings--i-i-spec.js
@@ -1,73 +1,39 @@
-'use babel';
-
-import LanguageCrusaderKings-I from '../lib/language-crusader-kings--i-i';
-
-// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
-//
-// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
-// or `fdescribe`). Remove the `f` to unfocus the block.
-
-describe('LanguageCrusaderKings-I', () => {
-  let workspaceElement, activationPromise;
+describe("CK2 grammar", function() {
+  var grammar = null;
+  var root = "source.ck2"; // defined here to allow painless changes (and reduce typing)
 
   beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('language-crusader-kings--i-i');
-  });
-
-  describe('when the language-crusader-kings--i-i:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.language-crusader-kings--i-i')).not.toExist();
-
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'language-crusader-kings--i-i:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.language-crusader-kings--i-i')).toExist();
-
-        let languageCrusaderKings-IElement = workspaceElement.querySelector('.language-crusader-kings--i-i');
-        expect(languageCrusaderKings-IElement).toExist();
-
-        let languageCrusaderKings-IPanel = atom.workspace.panelForItem(languageCrusaderKings-IElement);
-        expect(languageCrusaderKings-IPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'language-crusader-kings--i-i:toggle');
-        expect(languageCrusaderKings-IPanel.isVisible()).toBe(false);
-      });
+    waitsForPromise(() => {
+      return atom.packages.activatePackage("language-crusader-kings--i-i");
     });
 
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.language-crusader-kings--i-i')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'language-crusader-kings--i-i:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let languageCrusaderKings-IElement = workspaceElement.querySelector('.language-crusader-kings--i-i');
-        expect(languageCrusaderKings-IElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'language-crusader-kings--i-i:toggle');
-        expect(languageCrusaderKings-IElement).not.toBeVisible();
-      });
+    runs(() => {
+      grammar = atom.grammars.grammarForScopeName(root);
     });
   });
+
+  it("parses the grammar", () => {
+    expect(grammar).toBeTruthy();
+    expect(grammar.scopeName).toBe(root);
+  });
+
+  it("is configured correctly", () => {
+    expect(grammar.maxLineLength).toBe(Infinity);
+  });
+
+  it("tokenizes spaces", () => {
+    let tokens = grammar.tokenizeLine(" ").tokens;
+    expect(tokens[0]).toEqual({value: " ", scopes:[root]});
+  });
+
+  it("tokenizes comments", () => {
+    let tokens = grammar.tokenizeLine("#comment").tokens;
+    expect(tokens[0]).toEqual({value: "#comment", scopes:[root, "comment.line.number-sign.ck2"]});
+  });
+
+  it("tokenizes the always keyword", () => {
+    let tokens = grammar.tokenizeLine("always").tokens;
+    expect(tokens[0]).toEqual({value: "always", scopes: [root, "constant.language.bool.true.always.ck2"]});
+  });
+
 });
--- spec/language-crusader-kings--i-i-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageCrusaderKings-IView from '../lib/language-crusader-kings--i-i-view';
-
-describe('LanguageCrusaderKings-IView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
